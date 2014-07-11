2a7235939babeed595a530b5e326ea2500180495
AlexLaviolette <alex.laviolette92@gmail.com>
2014-07-11 16:08:45 -0400

    update

--- rcsmap.cpp
+++ rcsmap.cpp
@@ -9,6 +9,8 @@
 #define SYN_BIT 0x4
 #define ACK_BIT 0x2
 #define FIN_BIT 0x1
+#define MSS USHRT_MAX
+#define MAX_DATA_SIZE (MSS - HEADER_LEN)
 
 RcsConn::RcsConn(): ucp_sock(ucpSocket()), seq_num(0) {}
 
@@ -59,7 +61,7 @@ int RcsConn::accept(sockaddr_in * addr, RcsMap & map) {
 
     set_flags(request, SYN_BIT | ACK_BIT);
     set_checksum(request);
-    
+
     unsigned int conn_sock = newConn.second.getSocketId();
     while (1) {
         ucpSendTo(conn_sock, request, HEADER_LEN, addr);
@@ -105,11 +107,66 @@ int RcsConn::connect(const sockaddr_in * addr) {
 }
 
 int RcsConn::recv(void * buf, int maxBytes) {
+    ucpSetSockRecvTimeout(ucp_sock, CONNECT_TIMEOUT);
+
+    sockaddr_in addr;
+    int amount_recvd = 0;
+
+    char* request = new char[maxBytes + HEADER_LEN];
+    if (!request) return -1;
+
+    while(1) {
+        amount_recvd = ucpRecvFrom(ucp_sock, request, maxBytes + HEADER_LEN, &addr);
+        if (errno & (EAGAIN | EWOULDBLOCK)) continue;
+        if (is_corrupt(request) || get_seq_num(request) != seq_num) {
+            char response[HEADER_LEN] = {0};
+            set_flags(response, ACK_BIT);
+            set_seqnum(response, seq_num);
+            set_checksum(response);
+            ucpSendTo(ucp_sock, response, HEADER_LEN, &addr);
+            continue;
+        }
+        break;
+    }
+
+    memcpy(buf, response + HEADER_LEN, amount_recvd - HEADER_LEN);
+
+    char response[HEADER_LEN] = {0};
+    set_flags(response, ACK_BIT);
+    set_seqnum(response, seq_num);
+    set_checksum(response);
+    ucpSendTo(ucp_sock, response, HEADER_LEN, &addr);
+
+    seq_num++;
+    return amount_recvd;
+
 
 }
 
 int RcsConn::send(const void * buf, int numBytes) {
+    const char * charBuf = (const char *) buf;
+    unsigned int packet_seq_num = 1;
+    for (const char * chunk = charBuf; chunk += MAX_DATA_SIZE; chunk - charBuf < numBytes) {
+        unsigned int dataSize = min(MAX_DATA_SIZE, chunk - charBuf);
+        char * packet = new char[dataSize + HEADER_LEN];
+        if (!packet) {
+            while (!queue.empty()) {
+                delete queue.front();
+                queue.pop();
+            }
+            return -1;
+        }
 
+        memcpy(packet + HEADER_LEN, chunk, dataSize);
+        memset(packet, 0, HEADER_LEN);
+
+        set_seqnum(packet, packet_seq_num);
+        set_length(packet, dataSize);
+        set_checksum(packet);
+
+        packet_seq_num++;
+        queue.push(packet);
+    }
 }
 
 int RcsConn::close() {
